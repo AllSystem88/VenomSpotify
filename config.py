92852cf6881f5c12bc7527614fb85405ecdeda98
AllSystem88 <deependray228@gmail.com>
2025-04-11 12:10:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,6 +49,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # ----------------------------------------------------------------
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/APNA_SYSTEM
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
